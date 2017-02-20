a254f2cfe625636f3be5d9d2b2d1dd43672e1b7d
svetasvesh <svetasvesh@yandex.ru>
2017-02-21 00:01:57 +0300

    collector was succesfully runned

--- main.cc
+++ main.cc
@@ -49,7 +49,7 @@ void load_gcc(){
 		--releasever=7 \
 		--nogpgcheck \
 		-y \
-		install gcc-gfortran.x86_64"
+		install gcc-c++.x86_64"
 	);
 
 	system(
@@ -64,7 +64,7 @@ void load_gcc(){
 		--releasever=7 \
 		--nogpgcheck \
 		-y \
-		install tar.x86_64 gzip.x86_64"
+		install zlib-devel.x86_64"
 	);
 	std::cout <<"Load make..." <<std::endl;
 	system(
@@ -74,17 +74,17 @@ void load_gcc(){
 		-y \
 		install make.x86_64"
 	);
+	// system(
+	// 	"yum --installroot=/home/svetasvesh/mnt/new_root \
+	// 	--releasever=7 \
+	// 	--nogpgcheck \
+	// 	-y \
+	// 	install mercurial"
+	// );
 }
 
 void load_uuid_lib() {
-	std::cout <<"Load uuid lib..." <<std::endl;
-	system("ls -l /dev/null");
-	system("rm -rf /tmp");
-	system("mkdir /tmp");
-	check(chdir("/tmp"));
-	//system("wget http://www.mirrorservice.org/sites/ftp.ossp.org/pkg/lib/uuid/uuid-1.6.2.tar.gz");
-	system("wget http://www.netlib.org/blas/blas-3.7.0.tgz");
-	system ("tar -xvzf blas-3.7.0.tgz");
+	std::cout <<"Load ..." <<std::endl;
 }
 
 void change_fs() {
@@ -93,50 +93,41 @@ void change_fs() {
 	const char* dirpath = "/lib";
 	const char* put_old = "/home/svetasvesh/mnt/new_root/put_old";
 	const char* new_root = "/home/svetasvesh/mnt/new_root";
+	const char* project_path = "/put_old/mnt/home/svetasvesh/collector/spec-factory";
 	
 
 	check(chdir(new_root));
 	check(mount("", "/", "none", MS_PRIVATE | MS_REC, NULL));
 	
 	check(mount("", "/proc", "proc", MS_NOEXEC | MS_NOSUID | MS_NODEV, NULL));
-	//check(mount("", "/dev", "devtmpfs", MS_NOSUID | MS_STRICTATIME, "mode=755"));
-	check(mount("/dev", "/home/svetasvesh/mnt/new_root/dev", "none", MS_BIND | MS_REC, NULL));
-
-	//system("/var/tmp/mount_dev");
+	check(mount("", "/dev", "tmpfs", MS_NOSUID | MS_STRICTATIME, "mode=755"));
+	check(system("mkdir /home/svetasvesh/mnt/new_root/tmp/spec-factory"));
+	
 
 
 	check(mount(new_root, new_root, "none", MS_BIND | MS_REC, NULL));
 	check(syscall(SYS_pivot_root, new_root, put_old));
+	std::cout <<"/put_old/mnt/home/svetasvesh/collector/spec-factory:" <<std::endl;
+	check(system("ls -la /put_old/mnt/home/svetasvesh/collector/spec-factory"));
+	check(mount(project_path, "/tmp/spec-factory", "none",  MS_BIND, NULL));
 
-	/*DIR* dir = opendir(dirpath);
-	std::cout <<"I open dir " <<dir <<std::endl;
-	dirent* dirp;
-	while ( (dirp=readdir(dir)) != NULL) {
-		std::cout <<"I read dir " <<dirp->d_name <<std::endl;
-	}*/
-	//check(closedir(dir));
-
-	check(umount2("/put_old", MNT_DETACH));
-	//system("cp -f /put_old/etc/resolv.conf /etc/resolv.conf");
-	//system("ln -sfL /put_old/proc /proc");
-	//system("ln -sfL /put_old/sys /sys");
-	//system("ln -sfL /put_old/dev /dev");
-
-//	system("id");
-	//check(mount("/put_old/sys", "/sys", "sysfs", 0, NULL));
-	
-
-	
+		
 }
 
 void make_lib() {
 	std::cout <<"Make lib..." <<std::endl;
-	check(chdir("/tmp/BLAS-3.7.0"));
-	system("ls -l /dev/null");
-	//system("./configure \
-	//	--prefix=/tmp/uuid");
+	std::cout <<"/tmp:" <<std::endl;
+	check(system("ls -la /tmp"));
+	check(chdir("/tmp/spec-factory/"));
+	std::cout <<"/tmp/spec-factory:" <<std::endl;
+	check(system("ls -la"));
 	system("make");
-	system("make check");
+	system("cd test;\
+		../bin/discovery server localhost:10001 &\
+		../bin/discovery server localhost:10002 &\
+		../bin/discovery client localhost:10000");
+	check(umount2("/put_old", MNT_DETACH));
+
 }
 
 int load_env(void* pid_size) {
@@ -150,9 +141,9 @@ int load_env(void* pid_size) {
         exit(EXIT_FAILURE);
     }
 
-	load_gcc();
+	//load_gcc();
 	change_fs();
-	load_uuid_lib();
+	//load_uuid_lib();
 	make_lib();
 	exit(0);
 }
@@ -161,7 +152,6 @@ void create_env() {
 	size_t stack_size = 1024*1024;
 	char* child_stack = new char[stack_size];
 	void* child_stack_end = child_stack + stack_size;
-	//int pid  = 0;
 
 	check(child_pid = clone(load_env, child_stack_end, CLONE_NEWPID | CLONE_NEWUSER | CLONE_NEWNS | SIGCHLD, 0));
 	new_user_maps(child_pid);
